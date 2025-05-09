2a3372d344c42f3b4eea5af2555bf9aaddf989fb
Eric Laufer <thiboeri@gmail.com>
2025-05-09 13:21:12 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="mcp",
+    version="1.6.0",  # Define the version here
+    description="Your project description",
+    packages=["mcp"],  # Replace with your actual package list
+)
