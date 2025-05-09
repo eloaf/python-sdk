b3d6aee3e85a476229fd03f757817e0921740772
Eric Laufer <thiboeri@gmail.com>
2025-05-09 13:22:52 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="mcp",
-    version="1.6.0",  # Define the version here
-    description="Your project description",
-    packages=["mcp"],  # Replace with your actual package list
-)
